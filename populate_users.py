2843049d32705a0980bfe80c1f8ac268577e1955
anku1412 <ankita.vats5973@gmail.com>
2020-04-21 18:10:46 +0530

    'second'

--- populate_users.py
+++ populate_users.py
@@ -33,6 +33,6 @@ def populate(N=5):
 
 
 if __name__ == '__main__':
-    print("Populating the databases...Please Wait")
+    print("Populating the databases.....Please Wait")
     populate(20)
     print('Populating Complete')
